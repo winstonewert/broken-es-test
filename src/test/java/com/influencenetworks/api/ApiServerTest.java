05606bddc095daa65812f54ca9b9ec25110ccca1
Winston Ewert <winstonewert@gmail.com>
2016-05-02 14:13:32 -0700

    changes

---
+++ src/test/java/com/influencenetworks/api/ApiServerTest.java
@@ -0,0 +1,13 @@
+package com.influencenetworks.api;
+
+import org.elasticsearch.test.ESIntegTestCase;
+import org.junit.Test;
+
+
+
+public class ApiServerTest extends ESIntegTestCase {
+
+	@Test
+	public void testQuery() throws Exception {
+	}
+}
